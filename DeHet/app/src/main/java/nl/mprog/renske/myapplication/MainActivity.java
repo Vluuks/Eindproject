fa5e527c79914ee5e01047f1619893c5e82c35ef
Vluuks <vluuks@gmail.com>
2016-01-06 14:43:06 +0100

    Woordkiezer, lidwoordcheck en scorefunctie toegevoegd.

--- DeHet/app/src/main/java/nl/mprog/renske/myapplication/MainActivity.java
+++ DeHet/app/src/main/java/nl/mprog/renske/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package nl.mprog.renske.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -12,10 +17,20 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    public ArrayList<String> keylist;
+    public TextView woordTextView, scoreTextView, livesTextView, multiplierTextView;
+    public String lidwoord, znw;
+    public Button de_button, het_button;
+    public int score, lives, multiplier;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +44,30 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        scoreTextView = (TextView) findViewById(R.id.scoreTextview);
+        livesTextView = (TextView) findViewById(R.id.livesTextview);
+        multiplierTextView = (TextView) findViewById(R.id.multiplierTextview);
+
+        initializeGame();
+
+
+        pickWord();
+
+
+
     }
 
+    public void initializeGame()
+    {
+        score = 0;
+        lives = 3;
+        multiplier = 0;
+
+        scoreTextView.setText(Integer.toString(score));
+        livesTextView.setText("LIVES " + Integer.toString(lives));
+        multiplierTextView.setText(" ");
+    }
     public void displayHintScreen(View view) {
         // would like to avoid starting a new activity if possible
     }
@@ -40,18 +77,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkArticle(View view) {
-        // would like to avoid starting a new activity if possible
+
+        if (view.getId() == R.id.de_button) {
+            if(lidwoord.equals("[de]")){
+                multiplier++;
+                multiplierTextView.setText("COMBO x" + Integer.toString(multiplier));
+            }
+
+            else {
+                lives--;
+                multiplier = 0;
+                multiplierTextView.setText(" ");
+            }
+        }
+        else if (view.getId() == R.id.het_button) {
+            if(lidwoord.equals("[het]")){
+                multiplier++;
+                multiplierTextView.setText("COMBO x" + Integer.toString(multiplier));
+            }
+
+            else {
+                lives--;
+                multiplier = 0;
+                multiplierTextView.setText(" ");
+            }
+        }
+
+        //calculate score increase
+        score = score + multiplier;
+
+        // update layout elements
+        scoreTextView.setText(Integer.toString(score));
+        livesTextView.setText("LIVES " + Integer.toString(lives));
+
+
+
+        if(lives > 0){
+            pickWord();
+        }
+        // else go to achievement screen/game over screen
+        else {
+            Toast toast = Toast.makeText(this, "HAHAHA GIT GUD MATE.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+            toast.show();
+
+            Intent intent = new Intent(this, HighscoreActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 
     // cannot be put in another class due to getassets not working?
-    public Map<String, String> map = new HashMap<String, String>();
+    public Map<String, String> dictionarymap = new HashMap<String, String>();
     public String dictValue, dictKey;
 
 
     public void loadDictionary()
-            throws XmlPullParserException, IOException
-    {
+            throws XmlPullParserException, IOException {
         // create the xmlpullparser and factory
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         factory.setNamespaceAware(false);
@@ -68,23 +151,63 @@ public class MainActivity extends AppCompatActivity {
             // get the name of the tag
             String name = xpp.getName();
 
-            switch (eventType){
+            switch (eventType) {
                 case XmlPullParser.START_TAG:
                     // if the tag's name is <k>
-                    if(name.equals("k")){
+                    if (name.equals("k")) {
                         if (xpp.next() == XmlPullParser.TEXT)
                             dictKey = xpp.getText();
-                            System.out.println(dictKey);
+                        System.out.println(dictKey);
                     }
                     break;
 
                 case XmlPullParser.END_TAG:
                     break;
             }
-            map.put(dictKey, dictValue);
+            dictionarymap.put(dictKey, dictValue);
             eventType = xpp.next();
         }
-        System.out.println("End document");
-        Log.d("TEST", "Blabla");
+        System.out.println("Finished loading dictionary");
+
+        // store keys in arraylist to facilitate random picking
+        keylist = new ArrayList<String>();
+
+        for (Map.Entry<String, String> entry : dictionarymap.entrySet()) {
+            String theWord = entry.getKey();
+            keylist.add(theWord);
+        }
+    }
+
+
+    public void pickWord() {
+        // pick a random key from the arraylist with keys
+        Random randomizer = new Random();
+        String pickedWord = keylist.get(randomizer.nextInt(keylist.size()));
+        System.out.println("Gekozen woord raw:" + pickedWord);
+        String pickedWordTranslation = dictionarymap.get(pickedWord);
+
+        // split keystring so that you have the article and the rest
+        String[] wordparts = pickedWord.split(" ", 2);
+        lidwoord = wordparts[0];
+        String znwparts = wordparts[1];
+
+        System.out.println("Bijbehorend lidwoord:" + lidwoord);
+        System.out.println("Znw:" + znwparts);
+
+        // if the dictionary entries comes with multiple nouns, only use the first
+        if(znwparts.indexOf(';') != -1) {
+            String[] znwpartsarray = znwparts.split(";", 2);
+            znw = znwpartsarray[0];
+        }
+
+        else
+            znw = znwparts;
+
+        woordTextView =(TextView) findViewById(R.id.woordTextview);
+        woordTextView.setText(znw);
+
+
+        System.out.println(znw);
+
     }
 }
--- PROCESS.md
+++ PROCESS.md
@@ -1,5 +1,16 @@
 ## Day 2
-* Begonnen aan design document
-* Bepaald welke methods and classes ik ga gebruiken
-* XML parser basis gemaakt (alleen Nederlands)
-* Basiselementen layout app gemaakt
+* Begonnen aan design document.
+* Bepaald welke methods and classes ik ga gebruiken.
+* XML parser basis gemaakt (vooralsnog alleen Nederlands).
+* Basiselementen layout app gemaakt. 
+
+
+## Day 3
+* Design document verder uitgewerkt: meer nagedacht over wat een handige indeling is qua classes.
+* Random item uit dictionary picker gemaakt na eerst de keys in een ArrayList te hebben gestopt.
+* dit item gesplitst in lid- en zelfstandig naamwoord en de mogelijk extra definities buiten beschouwing gelaten.
+* checkfunctie lidwoord en scoresysteem gemaakt en puntensysteem ingevoerd.
+* Toch gekozen voor een vaste hoeveelheid levens om de achievements betekenisvol te houden.
+* Ga nu een timer of teller invoeren zodat er toch een einde is aan de app ook al doe je alles goed.
+* Vertaling als je er op tikt staat even on hold ivm XMLparser die veel problemen geeft. Ga eerst verder met het echte spel uitwerken.
+* Ontdekt dat er soms woorden inzitten waarbij zowel de als het goed zouden zijn. Bijvoorbeeld stof. Deze ga ik er waarschijnlijk uithalen.
